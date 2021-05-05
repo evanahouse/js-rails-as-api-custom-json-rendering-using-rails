03fce9dd65ab4a75747b93fdc4c0fae3b2650800
Evan <evanahouse@gmail.com>
2021-05-05 15:34:14 -0700

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,29 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
   end
+  # most verbose
+  # 
+  # def show
+  #   bird = Bird.find_by(id: params[:id])
+  #   render json: {id: bird.id, name: bird.name, species: bird.species}
+  # end
+  
+  #  slice only works for single hashes, not nested one
+  
+  # def show
+  #   bird = Bird.find_by(id: params[:id])
+  #   render json: bird.slice(:id, :name, :species)
+  # end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: bird, only: [:id, :name, :species]
+    else
+      render json: { message: 'Bird not found' }  
+    end
+  end
+
 end
